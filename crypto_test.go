689a5299db89e2a2484b4a8333c6c6fc6042a740
Andrew Ayer <andrew@sslmate.com>
2015-12-16 14:05:49 -0800

    x/crypto/pkcs12: Add pbEncrypt function

--- crypto_test.go
+++ crypto_test.go
@@ -51,6 +51,40 @@ func TestPbDecrypterFor(t *testing.T) {
 	}
 }
 
+func TestPbEncrypterFor(t *testing.T) {
+	params, _ := asn1.Marshal(pbeParams{
+		Salt:       []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		Iterations: 2048,
+	})
+	alg := pkix.AlgorithmIdentifier{
+		Algorithm: asn1.ObjectIdentifier([]int{1, 2, 3}),
+		Parameters: asn1.RawValue{
+			FullBytes: params,
+		},
+	}
+
+	pass, _ := bmpString("Sesame open")
+
+	_, _, err := pbEncrypterFor(alg, pass)
+	if _, ok := err.(NotImplementedError); !ok {
+		t.Errorf("expected not implemented error, got: %T %s", err, err)
+	}
+
+	alg.Algorithm = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 12, 1, 3})
+	cbc, _, err := pbEncrypterFor(alg, pass)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+
+	expectedM := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+	M := []byte{185, 73, 135, 249, 137, 1, 122, 247}
+	cbc.CryptBlocks(M, M)
+
+	if bytes.Compare(M, expectedM) != 0 {
+		t.Errorf("expected M to be '%d', but found '%d", expectedM, M)
+	}
+}
+
 var pbDecryptTests = []struct {
 	in            []byte
 	expected      []byte
@@ -104,6 +138,39 @@ func TestPbDecrypt(t *testing.T) {
 	}
 }
 
+func TestPbEncrypt(t *testing.T) {
+	tests := [][]byte{
+		[]byte("A secret!"),
+		[]byte("A secret"),
+	}
+	expected := [][]byte{
+		[]byte("\x33\x73\xf3\x9f\xda\x49\xae\xfc\xa0\x9a\xdf\x5a\x58\xa0\xea\x46"), // 7 padding bytes
+		[]byte("\x33\x73\xf3\x9f\xda\x49\xae\xfc\x96\x24\x2f\x71\x7e\x32\x3f\xe7"), // 8 padding bytes
+	}
+
+	for i, c := range tests {
+		td := testDecryptable{
+			algorithm: pkix.AlgorithmIdentifier{
+				Algorithm: asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 12, 1, 3}), // SHA1/3TDES
+				Parameters: pbeParams{
+					Salt:       []byte("\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8"),
+					Iterations: 4096,
+				}.RawASN1(),
+			},
+		}
+		p, _ := bmpString("sesame")
+
+		err := pbEncrypt(&td, c, p)
+		if err != nil {
+			t.Errorf("error encrypting %d: %v", c, err)
+		}
+
+		if bytes.Compare(td.data, expected[i]) != 0 {
+			t.Errorf("expected %d to be encrypted to %d, but found %d", c, expected[i], td.data)
+		}
+	}
+}
+
 type testDecryptable struct {
 	data      []byte
 	algorithm pkix.AlgorithmIdentifier
@@ -111,6 +178,7 @@ type testDecryptable struct {
 
 func (d testDecryptable) Algorithm() pkix.AlgorithmIdentifier { return d.algorithm }
 func (d testDecryptable) Data() []byte                        { return d.data }
+func (d *testDecryptable) SetData(data []byte)                { d.data = data }
 
 func (params pbeParams) RawASN1() (raw asn1.RawValue) {
 	asn1Bytes, err := asn1.Marshal(params)
