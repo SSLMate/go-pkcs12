a933b1ed8842ba2e2e4ad4103337f4b532d10233
Andrew Ayer <andrew@sslmate.com>
2019-02-09 11:50:04 -0800

    Update copyright notices

--- mac_test.go
+++ mac_test.go
@@ -1,3 +1,4 @@
+// Copyright 2015, 2018, 2019 Opsmate, Inc. All rights reserved.
 // Copyright 2015 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
